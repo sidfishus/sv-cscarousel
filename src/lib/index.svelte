397c4fe8adedfc12848d19e2b2aa949fb7bd8e41
Sid <sidnet@gmail.com>
2025-03-15 16:07:18 +0000

    Publish and import package working.

---
+++ src/lib/index.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+</script>
+
+<!--
+<div style={{position: "relative"}}>
+    <div ref={carouselRef} className={"CarouselContainer"}
+         onScroll={CarouselScroll}>
+
+        {files.map((_,idx) => {
+            return (
+                <GalleryFileComponent
+                    idx={idx} state={fileState} SetState={SetFileState} key={idx}
+                    loadingNonState={fileNonState} carouselRef={carouselRef.current} mainProps={props}
+                />
+            );
+        })}
+    </div>
+
+    {showChevrons &&
+    <>
+        <img src={FileFullUrl(props, chevronUrl)}
+             className={getChevronClass(true)}
+             onClick={ScrollLeft}
+        />
+        <img src={FileFullUrl(props, chevronUrl)}
+             className={getChevronClass(false)}
+             onClick={ScrollRight}
+        />
+    </>
+    }
+</div>
+-->
+
+
+<div>
+
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,15 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<script lang="ts">
+    import Carousel from "$lib/index.svelte";
+</script>
+
+<style>
+    .carousel-container {
+        height: 600px;
+        width: 600px;
+    }
+</style>
+
+<div class="carousel-container">
+    <Carousel />
+</div>
